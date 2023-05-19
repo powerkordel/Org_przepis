36d2f7f3d476c1809260cdb69670716ec55dfa58
Artur Maciejczyk <maciejczyk.art@gmail.com>
2023-05-20 00:18:29 +0200

    Dodaj filtrowanie i zmień layout strony

--- Org_Przepis/Przepisy/formularze.py
+++ Org_Przepis/Przepisy/formularze.py
@@ -1,6 +1,4 @@
 from django.forms import ModelForm
-from django.http import HttpResponseRedirect
-
 from .models import Przepis
 
 
--- Org_Przepis/Przepisy/views.py
+++ Org_Przepis/Przepisy/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.http import HttpResponse, HttpRequest
-from .models import Przepis, User
+from .models import Przepis, User, PrzepisSkladnik, Kategoria, Skladnik
 import os
 from Org_Przepis import settings
 from .formularze import PrzepisForm
@@ -18,11 +18,53 @@ def przepisy(request):
         Przepis.delete()
     uzytkownik = User.objects.get(pk=request.user.pk)
     wszystkie_przepisy = Przepis.objects.filter(autor=uzytkownik)
+    kategoria = '*'
+    skladnik = '*'
+    slowo_klucz = ''
+    if request.method == 'POST':
+        filtrowane_przepisy = []
+        post = request.POST
+        kategoria = post.get('kategoria')
+        skladnik = post.get('skladnik')
+        slowo_klucz = post.get('slowo_klucz')
+        for p in wszystkie_przepisy:
+            dodac = True
+            if kategoria is not '*':
+                kategorie = p.kategorie.all()
+                ma_kategorie = False
+                for k in kategorie:
+                    ma_kategorie |= kategoria == k.nazwa
+                dodac = ma_kategorie
+            if skladnik is not '*':
+                s = Skladnik.objects.get(nazwa=skladnik)
+                ps = PrzepisSkladnik.objects.filter(przepis=p, składnik=s)
+                if len(ps) == 0:
+                    dodac = False
+            if slowo_klucz is not '':
+                pasuje = False
+                pasuje |= slowo_klucz.lower() in p.nazwa.lower()
+                pasuje |= slowo_klucz.lower() in p.opis.lower()
+                dodac = pasuje
+            if dodac:
+                filtrowane_przepisy.append(p)
+        wszystkie_przepisy = filtrowane_przepisy
+
     for przepis in wszystkie_przepisy:
         przepis.opis_podzielony = przepis.opis.split('\n')
-        przepis.k = przepis.kategorie.all()
+        przepis.wszystkie_kategorie = przepis.kategorie.all()
+        przepis.wszystkie_skladniki = []
+        for przepisSkladnik in PrzepisSkladnik.objects.filter(przepis=przepis):
+                przepis.wszystkie_skladniki.append(przepisSkladnik)
+
+    kategorie = Kategoria.objects.all()
+    skladniki = Skladnik.objects.all()
     dane = {
         'przepisy': wszystkie_przepisy,
+        'kategorie': kategorie,
+        'skladniki': skladniki,
+        'kategoria': kategoria,
+        'skladnik': skladnik,
+        'slowo_klucz': slowo_klucz,
     }
     return render(request, 'lista_przepisow.html', dane)
 
@@ -49,8 +91,9 @@ def dodaj_przepis(request):
         formularz = PrzepisForm(instance=przepis)
     dane = {
         'form': formularz,
-        'tytul_formularza': 'Dodaj przepis',
-        'tresc_przycisku': 'Dodaj'
+        'tytul_strony': 'Dodaj przepis',
+        'tresc_przycisku': 'Dodaj',
+        'id_formularza': 'formularz_dodaj_przepis',
     }
     return render(request, 'generyczny_formularz.html', dane)
 
@@ -62,7 +105,6 @@ def edytuj_przepis(request, pk):
         if formularz.is_valid():
             formularz.autor = User.objects.get(pk=request.user.pk)
             nowy_przepis = formularz.save()
-            # nowy_przepis.autor = User.objects.get(id=request.user.id)
             nowy_przepis.save()
             return redirect('przepisy')
     else:
@@ -70,34 +112,45 @@ def edytuj_przepis(request, pk):
         formularz = PrzepisForm(instance=przepis)
     dane = {
         'form': formularz,
-        'tytul_formularza': 'Edytuj przepis',
+        'tytul_strony': 'Edytuj przepis',
         'tresc_przycisku': 'Zapisz',
+        'id_formularza': 'formularz_edytuj_przepis',
     }
     return render(request, 'generyczny_formularz.html', dane)
 
 
 def zdjecie(request: HttpRequest, zdjecie: str) -> HttpResponse:
-    _, rozszerzenie = os.path.splitext(zdjecie)
-    sciezka = request.path
-    return zwroc_zdjecie(sciezka, rozszerzenie)
+    if request.user.is_anonymous:
+        odpowiedz = HttpResponse()
+        odpowiedz.status_code = 403
+    else:
+        _, rozszerzenie = os.path.splitext(zdjecie)
+        sciezka = request.path
+        odpowiedz = zwroc_zdjecie(sciezka, rozszerzenie)
+    return odpowiedz
 
 
 def statyczne_pliki(request: HttpRequest, plik: str) -> HttpResponse:
     _, rozszerzenie = os.path.splitext(plik)
     sciezka = request.path
     if rozszerzenie == 'jpeg' or rozszerzenie == 'jpg' or rozszerzenie == 'png':
-        return zwroc_zdjecie(sciezka, rozszerzenie)
+        odpowiedz = zwroc_zdjecie(sciezka, rozszerzenie)
     else:
-        return HttpResponse([], status_code=404)
+        odpowiedz = HttpResponse()
+        odpowiedz.status_code = 404
+    return odpowiedz
 
 
 def zwroc_zdjecie(sciezka: str, rozszerzenie: str) -> HttpResponse:
     try:
         with open(f'{settings.BASE_DIR}{sciezka}', "rb") as obrazek:
             bajty_obrazka = obrazek.read()
-            return HttpResponse(bajty_obrazka, content_type=f'image/{rozszerzenie}')
+            odpowiedz = HttpResponse(bajty_obrazka, content_type=f'image/{rozszerzenie}')
+            odpowiedz.status_code = 200
     except FileNotFoundError:
-        return HttpResponse([], status_code=404)
+        odpowiedz = HttpResponse()
+        odpowiedz.status_code = 404
+    return odpowiedz
 
 def register_view(request):
     # obsługa formularza rejestracji
@@ -114,6 +167,7 @@ def register_view(request):
         'form': form,
         'tytul_formularza': 'Rejestracja',
         'tresc_przycisku': 'Zarejestruj się',
+        'id_formularza': 'formularz_rejestracja',
     }
     return render(request, 'generyczny_formularz.html', dane)
 
@@ -133,6 +187,7 @@ def login_view(request):
         'form': form,
         'tytul_formularza': 'Logowanie',
         'tresc_przycisku': 'Zaloguj się',
+        'id_formularza': 'formularz_logowanie',
     }
     return render(request, 'generyczny_formularz.html', dane)
 
